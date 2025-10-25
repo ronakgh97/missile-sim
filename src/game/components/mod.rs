3c64756197cd39e235911ff53257a259d7e87017
Rounak Gh <ronakgh97@gmail.com>
2025-10-25 22:24:50 +0530

    Remove game module and related exports from lib.rs -> "I gave up"

--- src/game/components/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod aircraft;
-pub mod controller;
-pub mod health;
-pub mod input;
-pub mod marker;
-pub mod missile;
-pub mod movement;
-pub mod player;
--- src/game/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod bundles;
-pub mod components;
-
-pub mod prelude {
-    pub use crate::game::bundles::*;
-    pub use crate::game::components::*;
-}
